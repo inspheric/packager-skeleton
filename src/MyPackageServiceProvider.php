2070f2a4060be5d0ac80759c5381f19474b49d94
Kit Burton-Senior <mail@kitbs.com>
2018-09-06 15:29:26 +0100

    push sphere skeleton

--- src/MyPackageServiceProvider.php
+++ src/MyPackageServiceProvider.php
@@ -14,35 +14,22 @@ class :uc:packageServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadTranslationsFrom(__DIR__.'/../resources/lang', ':lc:vendor');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', ':lc:vendor');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
 
-        // Publishing is only necessary when using the CLI.
         if ($this->app->runningInConsole()) {
 
-            // Publishing the configuration file.
             $this->publishes([
-                __DIR__.'/../config/:lc:package.php' => config_path(':lc:package.php'),
+                __DIR__.'/../config/config.php' => config_path(':lc:package.php'),
             ], ':lc:package.config');
 
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => base_path('resources/views/vendor/:lc:vendor'),
-            ], ':lc:package.views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/:lc:vendor'),
-            ], ':lc:package.assets');*/
-
-            // Publishing the translation files.
             $this->publishes([
                 __DIR__.'/../resources/lang' => resource_path('lang/vendor/:lc:vendor'),
             ], ':lc:package.lang');
 
-            // Registering package commands.
-            $this->commands([]);
+            $this->commands([
+                //
+            ]);
         }
     }
 
@@ -53,9 +40,8 @@ class :uc:packageServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/:lc:package.php', ':lc:package');
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', ':lc:package');
 
-        // Register the service the package provides.
         $this->app->singleton(':lc:package', function ($app) {
             return new :uc:package;
         });
